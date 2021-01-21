407e47f825d25b381b2965cda9f9848706407ab0
Anderson Rodrigues <anddersonrds@gmail.com>
2021-01-20 21:36:26 -0300

    Add Container Component

---
+++ src/components/Container/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  height: 100%;
+  width: 100%;
+  padding: 20px;
+  background: #3179ba;
+`;
