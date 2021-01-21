5de8537ec8db33370cb8706c767c50e27549cacb
Anderson Rodrigues <anddersonrds@gmail.com>
2021-01-20 21:37:54 -0300

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import Container from './components/Container';
+import Column from './components/Column';
+import Card from './components/Card';
+
 const App = () => (
-  <h1>Kanban React</h1>
-) 
+  <Container>
+    <Column text="To Do">
+      <Card text="Ganerate app scafold" />
+    </Column>
+
+    <Column text="In Progress">
+      <Card text="Learn Typescript" />
+    </Column>
+
+    <Column text="Done">
+      <Card text="Begin to use static typing" />
+    </Column>
+  </Container>
+);
 
 export default App;
